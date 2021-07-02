18def60aefa400154a51707a73210a48d6fc655a
toba-madamori <tobajamezmadamori@gmail.com>
2021-07-02 15:41:15 +0100

    signals to create profile upon new user registers

--- social/models.py
+++ social/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -22,4 +24,19 @@ class UserProfile(models.Model):
     birth_date = models.DateField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.png', blank= True)
-    
+
+'''    
+    sender - User
+    reciever - decorator (@reciever)
+    instance - User objects being saved
+    created - True/False
+'''    
+
+@receiver(post_save, sender= User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user= instance)
+
+@receiver(post_save, sender= User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
